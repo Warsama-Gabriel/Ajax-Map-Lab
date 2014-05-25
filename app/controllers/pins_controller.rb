45a2aae9fdc246bcf6e8be14442305857447d09c
chrome-tempest <srobleh@gmail.com>
2014-05-24 22:40:34 -0700

    handled json errors

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -15,8 +15,15 @@ class PinsController < ApplicationController
     respond_to do |format|
       if @pin.save
         format.json { render json: @pin, status: :created}
+      else
+        format.json { render json: @pin.errors, status: :unprocessable_entity }
       end
     end
   end
 
+  private
+
+  def pin_params
+    params.require(:pin).permit(:longitude, :latitude)
+  end
 end
