a26b567b39bdac0c36e6f8d3d9560fd2132b0d3f
chrome-tempest <srobleh@gmail.com>
2014-05-24 21:52:47 -0700

    added javascripts

--- app/assets/javascripts/pins.js
+++ app/assets/javascripts/pins.js
@@ -1,1 +1,21 @@
-pins.js
+
+$(document).ready(function(){
+
+  function initialize() {
+    var mapOptions = {
+    center: new google.maps.LatLng(-34.397, 150.644),
+    zoom: 8
+    };
+    
+    var map = new google.maps.Map(document.getElementById("map-canvas"),
+    mapOptions);
+  }
+  
+  google.maps.event.addDomListener(window, 'load', initialize);
+
+  var loc = new google.maps.LatLng(112.334, -21.572);
+  var newMarker = new google.maps.Marker({
+  position: loc,
+  map: map
+  });
+});
